2c842b46a25124af985d4f8c64d4808774ad0c3e
Thai Vo <dythirk@gmail.com>
2024-08-24 21:53:47 +0000

    revision

--- src/main/java/DoesNotContain.java
+++ src/main/java/DoesNotContain.java
@@ -17,7 +17,7 @@ public class DoesNotContain {
     public boolean arrayDoesNotContain(int[] arr, int target){
         boolean contains = false;
         for (int i = 0; i < arr.length; i++){
-            if ( arr[i] = target )
+            if ( arr[i] == target )
                 contains = true;
         }
         return !contains;
